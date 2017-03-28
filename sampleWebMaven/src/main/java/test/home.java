e0aef874f45529c37f01a767580121deda7fcac4
KishoreNegi <KishoreNegi@192.168.1.7>
2017-03-28 21:02:28 +0530

    Java servlet Sample

---
+++ sampleWebMaven/src/main/java/test/home.java
@@ -0,0 +1,46 @@
+package test;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class home
+ */
+@WebServlet("/home")
+public class home extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public home() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		request.getRequestDispatcher("/WEB-INF/views/first.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	//	doGet(request, response);
+		 
+		if (request.getParameter("username").equalsIgnoreCase("negi2002"))
+		request.getRequestDispatcher("/WEB-INF/views/success.jsp").forward(request, response);
+		else
+		request.getRequestDispatcher("/WEB-INF/views/failure.jsp").forward(request, response);
+	}
+
+}
---
+++ sampleWebMaven/src/main/webapp/WEB-INF/views/first.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Login Page</title>
+</head>
+<body>
+
+<form method="post" action="/home">
+
+Enter UserName <input type="text" name="username" value="">
+Enter Password <input type="text" name="password" value="">
+Click Button <input type="submit" name="Submit" value="">
+</form>
+
+</body>
+</html>
